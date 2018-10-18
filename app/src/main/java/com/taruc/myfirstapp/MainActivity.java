0cef963611a911ad23545f17aceba841f77f0f97
anreh <anurehka@gmail.com>
2018-10-18 11:33:38 +0800

    changes

--- app/src/main/java/com/taruc/myfirstapp/MainActivity.java
+++ app/src/main/java/com/taruc/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.taruc.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d( "Main Activity Alison", "Hello Log");
+        Log.d( "Main Activity Alison", "Hello Log 2");
 
         //initialize views
         buttonLogin = findViewById(R.id.buttonLogin);
